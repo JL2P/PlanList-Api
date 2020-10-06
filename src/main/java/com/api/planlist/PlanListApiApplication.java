45aa010038f8868475862b167d8be2268e531625
shoon2430 <shoon2430@gmail.com>
2020-10-07 08:27:30 +0900

    first commit

---
+++ src/main/java/com/api/planlist/PlanListApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.planlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanListApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/planlist/PlanListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.planlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
