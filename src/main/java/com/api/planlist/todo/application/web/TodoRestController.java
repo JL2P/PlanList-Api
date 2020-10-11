04e4935e187f5d72a8a0082f9afeef890f949467
shoon2430 <shoon2430@gmail.com>
2020-10-11 23:43:10 +0900

    Swagger 적용

--- src/main/java/com/api/planlist/todo/application/web/TodoRestController.java
+++ src/main/java/com/api/planlist/todo/application/web/TodoRestController.java
@@ -17,8 +17,11 @@ import com.api.planlist.todo.application.dto.TodoDTO;
 import com.api.planlist.todo.domain.Todo;
 import com.api.planlist.todo.domain.service.TodoService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
+@Api(tags = {"1. Todo"})
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/todos/")
@@ -28,17 +31,20 @@ public class TodoRestController {
 	
 	private final AccountService accountService;
 	
+	@ApiOperation(value="TODO 리스트 조회", notes="모든 TODO를 조회한다.")
 	@GetMapping()
 	public List<Todo> getTodos() {
 		return todoService.getTodos();
 	}
 	
+	@ApiOperation(value="TODO 디테일 정보 조회", notes="todoId값을 이용하여 조회한다.")
 	@GetMapping("{todoId}/")
 	public Todo getTodo(@PathVariable Long todoId) {
 		return todoService.getTodo(todoId);
 	}
 	
-
+	
+	@ApiOperation(value="TODO 추가", notes="TodoDTO타입을 이용하여 데이터를 받아온다.")
 	@PostMapping()
 	public Todo addTodo(@RequestBody TodoDTO todoDto) {
 		
@@ -54,11 +60,13 @@ public class TodoRestController {
 		return todoService.addTodo(newTodo);
 	}	
 	
+	@ApiOperation(value="TODO 수정", notes="작성되어있는 TODO를 수정한다.")
 	@PutMapping()
 	public Todo modifyTodo(@RequestBody Todo todo) {
 		return todoService.modifyTodo(todo);
 	}
 	
+	@ApiOperation(value="TODO 삭제", notes="TODO를 삭제한다.")
 	@DeleteMapping("{todoId}/")
 	public void deleteTodo(@PathVariable Long todoId) {
 		todoService.deleteTodo(todoId);
